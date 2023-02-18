ce114c9a01259acfe129a2e5aa4a5d33d623708e
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2023-02-18 12:09:06 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ryan DeVault on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
