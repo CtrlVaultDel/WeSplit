163509ad339f7d5de290729cddbee69ff4ee93b1
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2023-02-27 14:28:58 -0600

    Finished WeSplit project

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,24 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 20.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    let tipPercentages = [0, 10, 15, 20, 25]
+    var tipValue: Double{
+        return checkAmount / 100 * Double(tipPercentage)
+    }
+    var grandTotal: Double{
+        return checkAmount + tipValue
+    }
+    var totalPerPerson: Double{
+        return grandTotal / Double(numberOfPeople + 2)
+    }
     var body: some View {
-        VStack {
+        NavigationView{
             Form{
                 Section{
-                    Text("Item 6")
-                    Text("Item 7")
-                    Text("Item 8")
-                    Text("Item 9")
-                    Text("Item 10")
-                    Text("Item 11")
+                    TextField("Check Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2 ..< 100){
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(0 ..< 101, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    
+                    Text("Tip \(tipValue, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))").padding()
+                    
+                    Text("Total Cost \(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))").padding()
+                    
+                    Text("Per Person \(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))").padding()
+                } header: {
+                    Text("Calculate tip")
                 }
-            }.navigationTitle("Swift UI")
+            }.navigationTitle("WeSplit")
+                .toolbar{
+                    ToolbarItemGroup(placement: .keyboard){
+                        Spacer()
+                        
+                        Button("Done"){
+                            amountIsFocused = false
+                        }
+                    }
+                }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().previewLayout(.device).previewDevice("iPhone 14")
+        ContentView()
     }
 }
